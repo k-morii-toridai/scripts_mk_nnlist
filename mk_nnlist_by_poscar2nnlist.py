17168a8819fbd91af8b4c1d5470aec453351295e
k-morii-toridai <morii1132829@gmail.com>
2023-11-27 22:36:46 +0900

    updated comments.

--- mk_nnlist_by_poscar2nnlist.py
+++ mk_nnlist_by_poscar2nnlist.py
@@ -4,27 +4,27 @@ import time
 import shutil
 import subprocess
 from multiprocessing import Pool, cpu_count
+
 from tqdm import tqdm
 import numpy as np
 
 
 # nnlistを作成したいPOSCARファイルの親フォルダリスト（.npy）のパスをコマンドライン引数として受け取る
 args = sys.argv
-# load poscar existed folder list
-print("Now loading poscar_folder_path_list from .npy file...Please wait a half minite.")
+# load poscar existed folder path list
+print("Now loading poscar_folder_path_list from .npy file...")
 poscar_folder_path_list = np.load(args[1], allow_pickle=True)
 print("poscar_folder_path_list was loaded from .npy file!!!")
 print(f"len(poscar_folder_path_list): {len(poscar_folder_path_list)}")
 # casting PosixPath to str
 poscar_folder_path_list = [str(p) for p in poscar_folder_path_list]
-# get poscar2nnlist abs path
+# get the abs path of poscar2nnlist
 cwd = os.getcwd()
 poscar2nnlist_abs_path = cwd + '/neib_code/poscar2nnlist'
-# get poscar2nnlist 2nd arg
+# get 2nd arg of poscar2nnlist
 inputted_distance = input("Input a number which is poscar2nnlist's second arg. :nearest neighbors distance(Å) you want to catch: ")
 
 
-# poscar2nnlist = input()
 def cd_dir_and_pos2nnlist(poscar_folder_path):
     # 1. Change current dir to dir that exists a POSCAR file
     os.chdir(poscar_folder_path)
@@ -42,6 +42,7 @@ def cd_dir_and_pos2nnlist(poscar_folder_path):
     shutil.move(poscar_nnlist_p, poscar_nnlist_folder_p)
 
 
+# make POSCAR.nnlist by using cd_dir_and_pos2nnlist() and POSCAR files
 before = time.time()
 try:
     p = Pool(cpu_count() - 1)
